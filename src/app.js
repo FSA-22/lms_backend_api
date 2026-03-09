52647d8f324494083c13aa50cdaf224b1a573079
Promise <[infinitelaurels1@gmail.com]9mailto:infinitelaurels1@gmail.com)>
2026-03-09 22:09:38 +0100

    fix: resolved all merge conflicts, fixed env variables, and restored server

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import { CLIENT_URL, NODE_ENV, PORT } from './config/env.js';
 import AppError from './utils/appError.js';
 import errorMiddleware from './middlewares/errorMiddleware.js';
 
-import { CLIENT_URL, NODE_ENV } from './config/env.js';
+
 
 import superuserRouter from './routes/superUser.route.js';
 
@@ -71,6 +71,7 @@ app.use('/api/v1/', lessonContentRouter);
 app.use('/api/v1/', lessonAttachmentRouter);
 app.use('/api/v1/', studentAnswerRouter);
 
+app.get('/', (req, res) => {
   res.status(200).json({
     success: true,
     message: `API is running on port ${5000}`,
@@ -78,7 +79,7 @@ app.use('/api/v1/', studentAnswerRouter);
     timestamp: new Date(),
     serverIPs: ips
   });
-
+});
 
 //*404 Handler
 //app.use((req, res) => {
